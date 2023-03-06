1eb0913ac55aa32dbf826756ec9def10f135cb5d
MelsLores <melslores@outlook.es>
2023-03-05 23:25:02 -0600

    Splashscreen

--- app/src/main/java/com/example/natg/MainActivity.kt
+++ app/src/main/java/com/example/natg/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        Thread.sleep(500)
+        setTheme(R.style.Theme_NATG)
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
