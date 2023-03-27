e12e8d3bb825b4be17a1f22d1eb6e9a4f18604d8
MelsLores <melslores@outlook.es>
2023-03-27 16:12:19 -0600

    medio curso

--- app/src/main/java/com/example/natg/MainActivity.kt
+++ app/src/main/java/com/example/natg/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.natg.*
 import com.example.natg.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         setTheme(R.style.Theme_NATG)
-
+        auth = FirebaseAuth.getInstance()
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,32 +32,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         binding.button.setOnClickListener {
-            var username = findViewById<EditText>(R.id.eTusername)
-            var email = findViewById<EditText>(R.id.eTemail)
-            var password = findViewById<EditText>(R.id.eTpass)
-            var passconf = findViewById<EditText>(R.id.eTpassconf)
-            var registrar = findViewById<Button>(R.id.btnRegister)
-            var inicioSesion = findViewById<TextView>(R.id.txtInicio)
+            var email = findViewById<EditText>(R.id.emailEt)
+            var password = findViewById<EditText>(R.id.passET)
+            var inicioSesion = findViewById<Button>(R.id.button)
+            var regis = findViewById<Button>(R.id.button22)
             var validar: Int = 0
 
-            inicioSesion.setOnClickListener {
-                val intent: Intent = Intent(this, MainActivity::class.java)
+            regis.setOnClickListener {
+                val intent: Intent = Intent(this, registro::class.java)
                 startActivity(intent)
 
             }
 
 
-            registrar.setOnClickListener {
+            inicioSesion.setOnClickListener {
 
 
-                if (username.text.toString().isBlank()) {
-                    username.error = "No puede dejar campos vacíos."
-                    validar--
-                } else {
-                    validar++
-                    username.error = null
 
-                }
 
                 if (email.text.toString().isBlank()) {
                     email.error = "No puede dejar campos vacíos."
@@ -73,33 +66,11 @@ class MainActivity : AppCompatActivity() {
                     password.error = null
                 }
 
-                if (passconf.text.toString().isBlank()) {
-                    passconf.error = "No puede dejar campos vacíos."
-                    validar--
-                } else {
-                    validar++
-                    passconf.error = null
-                }
-
-                if (passconf.text.toString() != password.text.toString()) {
-                    passconf.error = "Las contraseñas no coinciden."
-                    password.error = "Las contraseñas no coinciden."
-                    validar--
-
-                } else {
-                    validar++
-                    passconf.error = null
-                    password.error = null
-
-                }
-
-                if (validar == 5) {
-                    var user = username.text.toString()
+                if (validar == 2) {
                     var mail = email.text.toString()
                     var contra = password.text.toString()
-                    var contraC = passconf.text.toString()
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(mail, contra)
+                    auth.signInWithEmailAndPassword(mail, contra)
                         .addOnCompleteListener {
                             if (it.isSuccessful) {
                                 val intent: Intent = Intent(this, inicio::class.java)
