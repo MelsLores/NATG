f1fc71f276d0b8b94c25bbe47297d492c861e915
MelsLores <melslores@outlook.es>
2023-03-31 17:42:56 -0600

    jetpack

--- app/src/main/java/com/example/natg/cafeteria.kt
+++ app/src/main/java/com/example/natg/cafeteria.kt
@@ -2,10 +2,12 @@ package com.example.natg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class cafeteria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cafeteria)
     }
+
 }
